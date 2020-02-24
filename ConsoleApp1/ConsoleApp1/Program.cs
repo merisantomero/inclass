539b2636cf36f70d79a6314e92d66ae7b7c80982
Meri Santomero <merisantomero@ou.edu>
2020-02-24 12:32:52 -0600

    random

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> examGrades = new List<double>();
+            
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Enter your exam grade. >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+                examGrades.Add(grade);
+
+                Console.WriteLine("Do you have another exam score, y or n?>>");
+                answer = Console.ReadLine();
+
+            } while (answer == "y");
+
+            int exams = (examGrades.Count);
+            double sum = 0;
+            double max = 0;
+            double min = 100;
+
+            for (int i = 0; i < exams; i++)
+            {
+                sum = sum + examGrades[i];
+                if (examGrades[i]>max)
+                {
+                    max = examGrades[i];
+                }
+                if (examGrades[i]<min)
+                {
+                    min = examGrades[i];
+                }
+            }
+            double average = sum / exams;
+
+            Console.WriteLine($"Your average exam score is {average}");
+            Console.WriteLine($"Your maximum exam score is {max}");
+            Console.WriteLine($"Your minimum exam score is {min}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exam1prep/exam1prep/Program.cs
@@ -0,0 +1,70 @@
+﻿//Meri Santomero
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam1prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> allDaNumbers = new List<double>();
+
+            //Console.WriteLine("Please enter a number. >>");
+            //string usersAnswerAsString = Console.ReadLine();
+            //double usersAnswerAsDouble = Convert.ToDouble(usersAnswerAsString);
+            //allDaNumbers.Add(usersAnswerAsDouble);
+
+            //Console.WriteLine("Do you want to enter another number? >>");
+            // string enterAnotherNumber = Console.ReadLine();
+
+            string usersAnswerAsString;
+            double usersAnswerAsDouble;
+            string enterAnotherNumber;
+            do
+            {
+                Console.WriteLine("Please enter a number. >>");
+                usersAnswerAsString = Console.ReadLine();
+                bool isAValidNumber;
+
+                isAValidNumber = double.TryParse(usersAnswerAsString, out usersAnswerAsDouble);
+
+                while (isAValidNumber == false)
+                {
+                    Console.WriteLine("Please enter a valid number. >>");
+                    usersAnswerAsString = Console.ReadLine();
+
+                    isAValidNumber = double.TryParse(usersAnswerAsString, out usersAnswerAsDouble);
+                }
+
+                //usersAnswerAsDouble = Convert.ToDouble(usersAnswerAsString);
+                allDaNumbers.Add(usersAnswerAsDouble);
+
+                if (usersAnswerAsString[usersAnswerAsString.Length-1]=='!') //very last character is a !
+
+                Console.WriteLine("Do you want to enter another number? >>");
+                enterAnotherNumber = Console.ReadLine();
+
+            } while (enterAnotherNumber.ToUpper()[0].ToString() == "Y");
+
+            double bankAccountBalance = 4.58;
+            string newString = usersAnswerAsString.Replace("dumb", "brilliant").Replace("brilliant", "dumb");
+            Console.WriteLine($"Your bank account balnce is {bankAccountBalance.ToString("C5")}.");
+
+            //while (enterAnotherNumber == "yes")
+            //{
+                //Console.WriteLine("Please enter a number. >>");
+                //usersAnswerAsString = Console.ReadLine();
+                //usersAnswerAsDouble = Convert.ToDouble(usersAnswerAsString);
+                //allDaNumbers.Add(usersAnswerAsDouble);
+
+                //Console.WriteLine("Do you want to enter another number? >>");
+                //enterAnotherNumber = Console.ReadLine();
+            //}
+
+        }
+    }
+}
